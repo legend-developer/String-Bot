a737d52b9eac08de155bacc88cbe7f0fb859bca8
legend-developer <jaishrir524@gmail.com>
2024-08-08 23:11:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID","14050586"))
-API_HASH = getenv("API_HASH","42a60d9c657b106370c79bb0a8ac560c")
+API_ID = int(getenv("API_ID","25028796"))
+API_HASH = getenv("API_HASH","885615da36b5b0128f86159f77a20a0e")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID"))
 
-MONGO_DB_URI = ("MONGO_DB_URI","mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN", None)
+MONGO_DB_URI = ("MONGO_DB_URI","mongodb+srv://shiv230708:shivam@23@cluster0.0awjqhr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MUST_JOIN = getenv("MUST_JOIN", "https://t.me/life_changing_movies")
